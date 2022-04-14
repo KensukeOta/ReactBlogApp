71526d7a881e3cdd42f242c0a641645e2460eed0
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-15 00:16:01 +0900

    ログインとログアウトのためのルーティングとコントローラーを記述 #1

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(LoginController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout');
 });
