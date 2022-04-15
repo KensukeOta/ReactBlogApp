70ca7bee3827217432ed6c377a349e9b6898b728
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-15 19:54:17 +0900

    ログイン状態とログイン中のユーザー情報を一元管理するためのRecoilを実装 #1

--- frontend/src/components/pages/Top.tsx
+++ frontend/src/components/pages/Top.tsx
@@ -1,9 +1,17 @@
+import { useRecoilValue } from "recoil";
+import { loginState } from "../../store/loginState";
 import { DefaultLayout } from "../templates/DefaultLayout";
 
 export const Top = () => {
+  const authUserStatus = useRecoilValue(loginState);
+  const isLogin = authUserStatus ? authUserStatus.isLogin : false;
+  console.log(isLogin)
+  
   return (
     <DefaultLayout>
       <h1>トップページ</h1>
+      <p>ログイン状態: {`${isLogin}`}</p>
+      <p>Welcome! {isLogin ? '' : 'stranger'}</p>
     </DefaultLayout>
   );
 };
