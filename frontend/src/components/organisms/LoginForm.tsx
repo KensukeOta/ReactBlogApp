645f77b3398d887d588e3044ca20caa625c2d567
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-16 11:20:27 +0900

    ログイン機能とログアウト機能を実装 #1

--- frontend/src/components/organisms/LoginForm.tsx
+++ frontend/src/components/organisms/LoginForm.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { axios } from "../../lib/axios";
+import { authUserInfo } from "../../store/authUserInfo";
+import { loginState } from "../../store/loginState";
 import { SubmitBtn } from "../atoms/SubmitBtn";
 import { EmailArea } from "../molecures/EmailArea";
 import { PasswordArea } from "../molecures/PasswordArea";
 
 export const LoginForm = () => {
+  const [error, setError] = useState();
+  
+  const [authUserStatus, setAuthUserStatus] = useRecoilState(loginState);
+  const isLogin = authUserStatus ? authUserStatus.isLogin : false;
+
+  const setUser = useSetRecoilState(authUserInfo);
+
+  const navigate = useNavigate();
+
   const {
     handleSubmit,
     register,
@@ -14,13 +29,23 @@ export const LoginForm = () => {
       password: ""
     }
   });
+
   const onSubmit = async (data: any) => {
-    console.log(data);
+    try {
+      await axios.get('http://localhost:8080/sanctum/csrf-cookie');
+      const res = await axios.post('http://localhost:8080/login', { email: data.email, password: data.password });
+      navigate('/', { replace: true });
+      setAuthUserStatus({ isLogin: !authUserStatus.isLogin });
+      setUser(res.data);
+    } catch (error: any) {
+      setError(error.response.data.message);
+    }
   }
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <dl>
+        <p className="text-red-500">{error}</p>
         <EmailArea register={register} />
         <p className="text-red-500">{errors.email?.message}</p>
         <PasswordArea register={register} />
--- frontend/src/components/pages/Top.tsx
+++ frontend/src/components/pages/Top.tsx
@@ -1,17 +1,19 @@
 import { useRecoilValue } from "recoil";
+import { authUserInfo } from "../../store/authUserInfo";
 import { loginState } from "../../store/loginState";
 import { DefaultLayout } from "../templates/DefaultLayout";
 
 export const Top = () => {
   const authUserStatus = useRecoilValue(loginState);
   const isLogin = authUserStatus ? authUserStatus.isLogin : false;
-  console.log(isLogin)
+
+  const user = useRecoilValue(authUserInfo);
   
   return (
     <DefaultLayout>
       <h1>トップページ</h1>
       <p>ログイン状態: {`${isLogin}`}</p>
-      <p>Welcome! {isLogin ? '' : 'stranger'}</p>
+      <p>Welcome! {user ? user.name : 'stranger'}</p>
     </DefaultLayout>
   );
 };
