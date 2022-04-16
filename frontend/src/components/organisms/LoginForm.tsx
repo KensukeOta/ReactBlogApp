a6064d20b928d61c984f41d040de182389544065
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-16 11:36:10 +0900

    ログイン時にログインページに遷移しようとしたらトップページへリダイレクトするように実装 #1

--- frontend/src/components/organisms/LoginForm.tsx
+++ frontend/src/components/organisms/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState, useSetRecoilState } from "recoil";
@@ -18,6 +18,12 @@ export const LoginForm = () => {
   const setUser = useSetRecoilState(authUserInfo);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLogin) {
+      navigate('/', { replace: true });
+    }
+  }, []);
 
   const {
     handleSubmit,
