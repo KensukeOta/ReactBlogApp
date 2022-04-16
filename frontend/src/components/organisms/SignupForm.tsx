994f0694af9867115d5f1855b12c2b33eabbccd6
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-16 15:11:00 +0900

    ユーザー登録機能を実装 #3

--- frontend/src/components/organisms/SignupForm.tsx
+++ frontend/src/components/organisms/SignupForm.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { axios } from "../../lib/axios";
+import { authUserInfo } from "../../store/authUserInfo";
+import { loginState } from "../../store/loginState";
 import { SubmitBtn } from "../atoms/SubmitBtn";
 import { EmailArea } from "../molecures/EmailArea";
 import { NameArea } from "../molecures/NameArea";
@@ -6,6 +12,15 @@ import { PasswordArea } from "../molecures/PasswordArea";
 import { PasswordConfirmArea } from "../molecures/PasswordConfirmArea";
 
 export const SignupForm = () => {
+  const [error, setError] = useState();
+
+  const [authUserStatus, setAuthUserStatus] = useRecoilState(loginState);
+  const isLogin = authUserStatus ? authUserStatus.isLogin : false;
+
+  const setUser = useSetRecoilState(authUserInfo);
+
+  const navigate = useNavigate();
+  
   const {
     handleSubmit,
     register,
@@ -22,15 +37,21 @@ export const SignupForm = () => {
 
   const onSubmit = async (data: any) => {
     try {
-      console.log(data);
-    } catch (error) {
-
+      await axios.get('http://localhost:8080/sanctum/csrf-cookie');
+      await axios.post('http://localhost:8080/api/register', { name: data.name, email: data.email, password: data.password, password_confirmation: data.password_confirmation });
+      const res = await axios.post('http://localhost:8080/login', { email: data.email, password: data.password });
+      navigate('/', { replace: true });
+      setAuthUserStatus({ isLogin: !authUserStatus.isLogin });
+      setUser(res.data);
+    } catch (error: any) {
+      setError(error.response.data.message);
     }
   };
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <dl>
+        <p className="text-red-500">{error}</p>
         <NameArea register={register} />
         <p className="text-red-500">{errors.name?.message}</p>
         <EmailArea register={register} />
