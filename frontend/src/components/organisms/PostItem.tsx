eaa803c43b8650933691ac3c2d65aabfffde964c
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-18 15:33:08 +0900

    記事削除機能を実装 #12

--- frontend/src/components/organisms/PostItem.tsx
+++ frontend/src/components/organisms/PostItem.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { authUserInfo } from "../../store/authUserInfo";
+import { DeleteBtn } from "../atoms/DeleteBtn";
 
 export const PostItem = (props: any) => {
   const user = useRecoilValue(authUserInfo);
@@ -10,7 +11,8 @@ export const PostItem = (props: any) => {
       <Link to={`/posts/${props.post.id}`}><h1 className="font-bold">{props.post.title}</h1></Link>
       <nav className="flex justify-between">
         <p>by {props.post.user.name}</p>
-        {user.id === props.post.user_id ? <Link to={`/posts/${props.post.id}/edit`}>更新する</Link>: null}
+        {user.id === props.post.user_id ? <Link to={`/posts/${props.post.id}/edit`}>更新する</Link> : null}
+        {user.id === props.post.user_id ? <DeleteBtn id={props.post.id} /> : null}
       </nav>
     </li>
   );
