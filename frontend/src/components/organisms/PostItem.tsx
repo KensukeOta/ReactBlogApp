a5eb3acbbf2180be0a0c105b6b488383c6a0bcbd
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-18 12:18:38 +0900

    記事詳細表示機能を実装 #8

--- frontend/src/components/organisms/PostItem.tsx
+++ frontend/src/components/organisms/PostItem.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export const PostItem = (props: any) => {
   return (
     <li className="border p-2">
-      <h1 className="font-bold">{props.post.title}</h1>
+      <Link to={`/posts/${props.post.id}`}><h1 className="font-bold">{props.post.title}</h1></Link>
       by {props.post.user.name}
     </li>
   );
