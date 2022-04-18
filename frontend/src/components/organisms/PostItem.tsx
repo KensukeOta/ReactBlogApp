0dbbe9dc9f1581dc5ad6b3f6582432331bb93cea
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-04-18 13:34:25 +0900

    更新フォームに遷移するリンクを実装 #10

--- frontend/src/components/organisms/PostItem.tsx
+++ frontend/src/components/organisms/PostItem.tsx
@@ -4,7 +4,10 @@ export const PostItem = (props: any) => {
   return (
     <li className="border p-2">
       <Link to={`/posts/${props.post.id}`}><h1 className="font-bold">{props.post.title}</h1></Link>
-      by {props.post.user.name}
+      <nav className="flex justify-between">
+        <p>by {props.post.user.name}</p>
+        <Link to={`/posts/${props.post.id}/edit`}>更新する</Link>
+      </nav>
     </li>
   );
 };
